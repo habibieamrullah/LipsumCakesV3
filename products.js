ee29c1e412217075ec8d26c9cc93b809d5d2e1c3
Habibie <habibieamrullah@gmail.com>
2020-08-03 22:51:29 +0700

    Correct product list

--- products.js
+++ products.js
@@ -2,16 +2,16 @@
 var products = [
     
     //Butter Cake
-    { cat : "Butter Cake", name : "Butter Cake" , price : "$35" , pricevalue : 35, image : "buttercake1" },
-    { cat : "Butter Cake", name : "Marble Butter Cake" , price : "$35" , pricevalue : 35, image : "buttercake2" },
+    { cat : "Butter Cake", name : "Butter Cake 1" , price : "$35" , pricevalue : 35, image : "buttercake1" },
+    { cat : "Butter Cake", name : "Butter Cake 2" , price : "$35" , pricevalue : 35, image : "buttercake2" },
     
     //Cupcake
-    { cat : "Cupcake", name : "Red Velvet" , price : "$4 each (minimum 6)" , pricevalue : 24, image : "cupcake1" },
-    { cat : "Cupcake", name : "Ondeh Ondeh" , price : "$4 each (minimum 6)" , pricevalue : 24, image : "cupcake2" },
+    { cat : "Cupcake", name : "Cupcake 1" , price : "$4 each (minimum 6)" , pricevalue : 24, image : "cupcake1" },
+    { cat : "Cupcake", name : "Cupcake 2" , price : "$4 each (minimum 6)" , pricevalue : 24, image : "cupcake2" },
     
     //Customised Cake
-    { cat : "Customised Cake", name : "Red Velvet with Cream Cheese Frosting" , price : "$55-$75" , pricevalue : 55, image : "custom1" },
-    { cat : "Customised Cake", name : "Chocolate Truffle Cake" , price : "$55-$75" , pricevalue : 55, image : "custom2" },
+    { cat : "Customised Cake", name : "Custom Cake 1" , price : "$55-$75" , pricevalue : 55, image : "custom1" },
+    { cat : "Customised Cake", name : "Custom Cake 2" , price : "$55-$75" , pricevalue : 55, image : "custom2" },
     
     
 ]
